b6eccd7e5b1e5b40ee3fd0e6a607ccba6eee657d
infodusha <infodusha@ya.ru>
2019-03-29 22:54:35 +0300

    New version. Now stable.

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-let { useState, useEffect, useRef } = require('react');
+const { useState, useEffect, useMemo } = require('react');
 
-let RObserver = window.ResizeObserver || require('resize-observer-polyfill').default;
+const RObserver = window.ResizeObserver || require('resize-observer-polyfill').default;
 
 module.exports.useSize = function useSize(ref) {
-    
-    let ro = useRef();
-    let [inited, setInited] = useState(false);
-    let [size, setSize] = useState({ width: Infinity, height: Infinity });
-    
-    let init = () => setInited(true);
+
+    const [ignored, setIgnored] = useState(0);
+    const [size, setSize] = useState({ width: Infinity, height: Infinity });
+
+    const forceUpdate = () => setIgnored((c) => c + 1);
+
+    const obs = useMemo(() => new RObserver((entries) => {
+        const { width, height } = entries[0].contentRect;
+        setSize((s) => s.width !== width || s.height !== height ? { width, height } : s);
+    }), []);
 
     useEffect(() => {
-        ro.current = new RObserver(entries => {
-            let { width, height } = entries[0].contentRect;
-            if(size.width !== width || size.height !== height)
-                setSize({ width, height });
-        });
-        window.setTimeout(init, 0);
-        return () => ro.current.disconnect();
-    }, [ref]);
-
-    useEffect(() => {
-        if(ro.current && ref.current) {
-            try {
-                ro.current.observe(ref.current);
-            } catch (e) {
-                throw new TypeError('useSize ref: is not on DOM element');
-            }
+        const item = ref.current;
+        if (item) {
+            obs.observe(item);
+            window.setTimeout(forceUpdate, 0);
         }
-    }, [inited]);
+        return () => item && obs.unobserve(item);
+    }, [obs, ref]);
 
     return size;
 
